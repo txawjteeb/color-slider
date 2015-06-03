25bedc94f0f4289c27010a5a3e459fe9ac7a8563
David Lee <itxawjteeb@gmail.com>
2015-06-02 20:25:29 -0700

    App that mixes RGB color values with slider

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by David Lee on 05/27/15.
+//  Copyright (c) 2015 David Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeColorComponent()
+    }
+    
+    @IBAction func changeColorComponent() {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
